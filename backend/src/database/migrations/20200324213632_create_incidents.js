fe1148732f031a48cd8b4d07aabc60384c7143f7
JAMaxwell <max.kawaii@hotmail.com>
2020-03-25 17:57:00 -0300

    Criação do backend.

---
+++ backend/src/database/migrations/20200324213632_create_incidents.js
@@ -0,0 +1,19 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable('incidents', function (table) {
+      table.increments(); //increments é automaticamente uma primary key
+
+      table.string('title').notNullable();
+      table.string('description').notNullable();
+      table.decimal('value').notNullable();
+
+      table.string('ong_id').notNullable();
+
+      table.foreign('ong_id').references('id').inTable('ongs'); //referencia outra tabela.
+    });
+  };
+  
+  exports.down = function(knex) {
+    return knex.schema.dropTable('incidents');
+  };
+  
